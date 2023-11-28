10bc97776f373cca8eca31d5effe5101f190ea9d
shivani mehta <shivanimehta21@navgurukul.org>
2023-11-28 22:16:45 +0530

    second data fetch

--- app/Phone.js
+++ app/Phone.js
@@ -1,7 +1,63 @@
+// import React, { useEffect, useState } from 'react';
+
+// const Phone = () => {
+//   const [phones, setPhones] = useState([]);
+
+//   useEffect(() => {
+//     const apiUrl = 'https://api.escuelajs.co/api/v1/products';
+
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch(apiUrl);
+
+//         if (!response.ok) {
+//           throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+
+//         const data = await response.json();
+//         // Limit to only 5 phones
+//         setPhones(data.slice(0, 5));
+//       } catch (error) {
+//         console.error('Error fetching data:', error.message);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div className="product-container">
+//       <h1 className="product-title">Featured Phones</h1>
+//       <div className="product-list">
+//         {phones.map((phone) => (
+//           <div key={phone.id} className="product-item">
+//             {phone.image && (
+//               <img
+//                 src={phone.image}
+//                 alt={phone.title}
+//                 className="product-image"
+//               />
+//             )}
+//             <h2 className="product-title">{phone.title}</h2>
+//             <p className="product-price">
+//               Price: {phone.price ? `$${phone.price.toFixed(2)}` : 'N/A'}
+//             </p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Phone;
+
+
+// Product.js or Phone.js
+
 import React, { useEffect, useState } from 'react';
 
-const Phone = () => {
-  const [products, setProducts] = useState([]);
+const Product = () => {
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
     const apiUrl = 'https://api.escuelajs.co/api/v1/products';
@@ -15,8 +71,8 @@ const Phone = () => {
         }
 
         const data = await response.json();
-        // Limit to only 5 products
-        setProducts(data.slice(0, 5));
+        // Limit to only 5 items
+        setItems(data.slice(0, 5));
       } catch (error) {
         console.error('Error fetching data:', error.message);
       }
@@ -29,18 +85,20 @@ const Phone = () => {
     <div className="product-container">
       <h1 className="product-title">Featured Products</h1>
       <div className="product-list">
-        {products.map((product) => (
-          <div key={product.id} className="product-item">
-            {product.image && (
+        {items.map((item) => (
+          <div key={item.id} className="product-item">
+            {item.image ? (
               <img
-                src={product.image}
-                alt={product.title}
+                src={item.image}
+                alt={item.title}
                 className="product-image"
               />
+            ) : (
+              <div className="product-no-image">No Image Available</div>
             )}
-            <h2 className="product-title">{product.title}</h2>
+            <h2 className="product-title">{item.title}</h2>
             <p className="product-price">
-              Price: {product.price ? `$${product.price.toFixed(2)}` : 'N/A'}
+              Price: {item.price ? `$${item.price.toFixed(2)}` : 'N/A'}
             </p>
           </div>
         ))}
@@ -49,4 +107,5 @@ const Phone = () => {
   );
 };
 
-export default Phone;
+export default Product;
+
